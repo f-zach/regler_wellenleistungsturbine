1810b599e492a5c7adb3ecb6521b44ea483c5647
f-zach <63615390+f-zach@users.noreply.github.com>
2022-11-21 16:25:28 +0100

    added communication with console and ecu pwm

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,20 @@ void learnLeverSettings();
 
 Servo ecuOut;
 
-movingAvg throttleAvg(10);
-movingAvg brakeAvg(10);
+movingAvg throttleAvg(50);
+movingAvg brakeAvg(50);
 
 char bufferArr[20] = {'1', '2'};
 String message = "";
 
 long tStart = 0, tBlink = 0;
-int potBrake = 0;
-int potThrottle = 0;
+int potBrake = 0, potThrottle = 0, engineRPM = 0, brakeRPM = 0;
 float power = 0.0;
-int rpm = 0;
-int setRPM = 100000;
+int setRPM = 0;
 float brakeSetting = 0.0;
 float throttleSetting = 0.0;
 int mode = 0;
-char c;
+char c, incomingChar;
 int pwmValve = 0;
 
 bool controllerOnSwitch, controllerOnSwitchLast, valueRecieved, valueConfirmed, valueDeclined;
@@ -35,12 +33,14 @@ String dataTransmission = "";
 void setup()
 {
   Serial.begin(9600);
+  Serial1.begin(115200);
   Serial5.begin(115200);
   pinMode(13, OUTPUT);
   pinMode(26, OUTPUT);
   pinMode(ecuPin, OUTPUT);
   pinMode(valvePin, OUTPUT);
   pinMode(27, INPUT);
+  pinMode(29, INPUT);
   pinMode(31, INPUT);
   pinMode(32, INPUT);
 
@@ -48,6 +48,7 @@ void setup()
   brakeAvg.begin();
 
   ecuOut.attach(ecuPin);
+  ecuOut.writeMicroseconds(0);
 
   digitalWrite(13, LOW);
 
@@ -92,31 +93,67 @@ void loop()
     }
   }
 
-  //Go to mode 1: Controller switch toggled on (wait for desired RPM input)
+  
+  if (Serial1.available() > 10)
+  {
+    delay(2);
+    Serial1.readStringUntil('*');
+    do
+    {
+      incomingChar = Serial1.read();
+      //Serial.println(incomingChar);
+      switch (incomingChar)
+      {
+      case 'r':
+        engineRPM = Serial1.readStringUntil(';').toFloat();
+        //Serial.println(engineRPM);
+        break;
+
+      case 's':
+        brakeRPM = Serial1.readStringUntil(';').toFloat();
+        //Serial.println(brakeRPM);
+        break;
+
+      case 'p':
+        power = Serial1.readStringUntil(';').toFloat();
+        //Serial.println(power);
+        break;
+
+      default:
+        break;
+      }
+      delayMicroseconds(10);
+    } while (incomingChar != '#');
+
+  }
+
+  
+
+  // Go to mode 1: Controller switch toggled on (wait for desired RPM input)
   if (controllerOnSwitch && !controllerOnSwitchLast)
   {
     digitalWrite(26, HIGH);
     mode = 1;
   }
-  //Go to moode 2: Controller switch on and waiting for user confirmation of RPM value
+  // Go to moode 2: Controller switch on and waiting for user confirmation of RPM value
   else if (controllerOnSwitch && controllerOnSwitchLast && valueRecieved)
   {
     mode = 2;
     valueRecieved = false;
   }
-  //Go to mode 3: Controller switch on and value confirmed by the user -> controller is activated
+  // Go to mode 3: Controller switch on and value confirmed by the user -> controller is activated
   else if (controllerOnSwitch && controllerOnSwitchLast && valueConfirmed)
   {
     mode = 3;
     valueConfirmed = false;
   }
-  //Go to mode 0: User declines RPM value
+  // Go to mode 0: User declines RPM value
   else if (controllerOnSwitch && controllerOnSwitchLast && valueDeclined)
   {
     mode = 0;
     valueDeclined = false;
   }
-  //Go to mode 0: Controller switch toggled off
+  // Go to mode 0: Controller switch toggled off
   else if (!controllerOnSwitch && controllerOnSwitchLast)
   {
     digitalWrite(26, LOW);
@@ -154,11 +191,15 @@ void loop()
     brakeSetting = (float(potBrake) - minBrakeValue - 30) / (maxBrakeValue - minBrakeValue - 60);
   }
 
-  ecuOut.writeMicroseconds(995 + int(throttleSetting * 1000));
+  if(digitalRead(29))
+  {
+    ecuOut.writeMicroseconds(995 + 115 + int(throttleSetting * 885));
+  }
+  else if(!digitalRead(29))
+  {
+    ecuOut.writeMicroseconds(995);
+  }
   analogWrite(valvePin, int(brakeSetting * 4096));
-
-  rpm = int(throttleSetting * 150000);
-  power = brakeSetting * 13;
 
   switch (mode)
   {
@@ -195,32 +236,35 @@ void loop()
   {
     dataTransmission = "";
     dataTransmission = "*";
-    dataTransmission += +"b" + String(brakeSetting) + ";";
-    dataTransmission += +"t" + String(throttleSetting) + ";";
+    dataTransmission += +"b" + String(brakeSetting,4) + ";";
+    dataTransmission += +"t" + String(throttleSetting,4) + ";";
+    dataTransmission += +"o" + String(int(brakeSetting * 4096)) + ";";
     dataTransmission += +"p" + String(power) + ";";
-    dataTransmission += +"r" + String(rpm) + ";";
+    dataTransmission += +"r" + String(brakeRPM) + ";";
     dataTransmission += +"s" + String(setRPM) + ";";
     dataTransmission += +"m" + String(mode) + ";";
     dataTransmission += +"#";
-    //Serial.println(dataTransmission);
+    Serial.println(dataTransmission);
     Serial5.println(dataTransmission);
     tStart = millis();
   }
+
+  //Serial.println(digitalRead(29));
 }
 
 void learnLeverSettings()
 {
   int reading = 0;
-  //Learn max throttle setting
-  if (analogRead(throttleLeverPin) < 3200)
-  {
-    Serial.println("Set throttle to full!");
-  }
+  // Learn max throttle setting
+  Serial.println("Set throttle to full!");
 
   while (analogRead(throttleLeverPin) < 3200)
   {
-    Serial5.println("!"+String(analogRead(throttleLeverPin)))
+    Serial5.println("*m4;n0;#");
   }
+
+
+  Serial.println("Wait!");
 
   int tStartReading = millis();
   int lastReading = 0;
@@ -231,19 +275,22 @@ void learnLeverSettings()
     {
       maxThrottleValue = reading;
     }
+    Serial5.println("*m5;n0;#");
   } while (millis() - tStartReading < 3000);
 
   Serial.println(maxThrottleValue);
+  Serial5.println("*m6;n0;#");
+  delay(1200);
 
-  //Read min throttle Setting
-  if (analogRead(throttleLeverPin) > 2500)
-  {
-    Serial.println("Set throttle to zero!");
-  }
+  // Read min throttle Setting
+  Serial.println("Set throttle to zero!");
 
   while (analogRead(throttleLeverPin) > 2500)
   {
+    Serial5.println("*m4;n1;#");
   }
+
+  Serial.println("Wait!");
 
   tStartReading = millis();
   lastReading = 9999;
@@ -254,19 +301,23 @@ void learnLeverSettings()
     {
       minThrottleValue = reading;
     }
+    Serial5.println("*m5;n1;#");
   } while (millis() - tStartReading < 3000);
 
   Serial.println(minThrottleValue);
+  Serial5.println("*m6;n1;#");
+  delay(1200);
 
-  //Set max brake setting
-  if (analogRead(brakeLeverPin) < 2800)
-  {
-    Serial.println("Set brake to full!");
-  }
+  // Set max brake setting
+
+  Serial.println("Set brake to full!");
 
   while (analogRead(brakeLeverPin) < 2800)
   {
+    Serial5.println("*m4;n2;#");
   }
+
+  Serial.println("Wait!");
 
   tStartReading = millis();
   lastReading = 0;
@@ -277,19 +328,22 @@ void learnLeverSettings()
     {
       maxBrakeValue = reading;
     }
+    Serial5.println("*m5;n2;#");
   } while (millis() - tStartReading < 3000);
 
   Serial.println(maxBrakeValue);
+  Serial5.println("*m6;n2;#");
+  delay(1200);
 
-  //Set min brake setting
-  if (analogRead(brakeLeverPin) > 2000)
-  {
-    Serial.println("Set brake to zero!");
-  }
+  // Set min brake setting
+  Serial.println("Set brake to zero!");
 
   while (analogRead(brakeLeverPin) > 2000)
   {
+    Serial5.println("*m4;n3;#");
   }
+
+  Serial.println("Wait!");
 
   tStartReading = millis();
   lastReading = 9999;
@@ -300,7 +354,11 @@ void learnLeverSettings()
     {
       minBrakeValue = reading;
     }
+    Serial5.println("*m5;n3;#");
   } while (millis() - tStartReading < 3000);
 
   Serial.println(minBrakeValue);
+  Serial5.println("*m6;n3;#");
+
+  delay(1200);
 }
